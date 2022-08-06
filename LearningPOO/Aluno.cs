a46edc51b291277799021b36ce5086a77a1c049c
ViniciusDamiani <vinidamiani03@hotmail.com>
2022-08-06 14:39:16 -0300

    Adicionar arquivos de projeto.

---
+++ LearningPOO/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningPOO
+{
+    //Classe
+    class Aluno
+    {
+        //Atributos
+        public static string Name = "";
+        public static string LongName = "";
+        public static int NrHeadquarters = 0;
+
+        public static int NotesAverage = 0;
+
+        //Metodo: Calcula Media
+        public void CalculateAverage(int Note1, int Note2, int Note3)
+        {
+            NotesAverage = Note1 + Note2 + Note3 / 3;
+        }
+
+        //Método: Verifica Situação
+        public void CheckedSituation() 
+        { 
+            if (NotesAverage < 7)
+            {
+                string MessageSituation = "Você reprovou! :(";
+            }
+            else
+            {
+                string MessageSituation = "Você passou! :)";
+            }
+        }
+
+
+
+    }
+}
---
+++ LearningPOO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando objeto aluno para receber os metodos de calcular média
+            Aluno aluno = new Aluno();
+
+            //Atribuindo valor a instancia da classe
+            Console.WriteLine("Informe seu nome: ");
+            Aluno.Name = Console.ReadLine();
+
+            Console.WriteLine("Informe sua primeira nota: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe sua segunda nota: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe sua terceira nota: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+
+            //Efetuado o metodo de calcular média
+            aluno.CalculateAverage(n1,n2,n3);
+            //Imprimindo a média do Aluno
+            Console.WriteLine("A sua média Sr."+ Aluno.Name + " foi: " + Aluno.NotesAverage);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
